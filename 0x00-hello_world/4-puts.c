a1a482a92e216ee30e7a965be7788e0f9ac06598
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-16 01:48:39 -0700

    Hello World Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Enrtry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
+}
