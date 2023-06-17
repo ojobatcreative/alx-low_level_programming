346ecbf8b8b15ee169648e91ffa7eb418a9960ab
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-16 22:36:27 -0700

    Prints the last digit of the number stored in the variable n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This prints the last digit of a random variable n
+ *
+ * Return: Always 0 (Succcess)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %d ", n, n % 10);
+
+	if (n % 10 > 5)
+
+		printf("and is greater that 5\n");
+
+	else if (n % 10 == 0)
+
+		printf("and is 0\n");
+
+	else if (n % 10 < 6 && n % 10 != 0)
+
+		printf("and is less than 6 and not 0\n");
+
+	return (0);
+}
