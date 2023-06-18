8b484e486ad00918c1ee9c204f29c5ba43afd7eb
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-17 23:12:49 -0700

    Prints an exact string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints the exact string " and that piece of art is useful"
+ * 	- Dora Korpar, 2015-10-19",
+ * 	followed by a new line, to the standard error.
  *
- * Return: Always 1 (Success)
- * */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
