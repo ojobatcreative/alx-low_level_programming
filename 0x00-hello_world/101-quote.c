1fe03fbce40fb255c654d66d9574040efed0f4be
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-17 23:20:34 -0700

    Prints an exact string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  * main - Prints the exact string " and that piece of art is useful"
- * 	- Dora Korpar, 2015-10-19",
- * 	followed by a new line, to the standard error.
+ *	- Dora Korpar, 2015-10-19",
+ *	followed by a new line, to the standard error.
  *
  * Return: Always 0 (Success)
  */
