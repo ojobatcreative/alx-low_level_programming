5d5f78024a897616374940229e5c09552ec5d46c
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-20 14:11:07 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @1: is the first value
+ * @2: is the second value
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
