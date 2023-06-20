58412a3f234caf3128b4cbf73777949d05064f76
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-20 07:52:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,13 @@
  * main - Entry point
  * description - Program to print _putchar
  * followed by a new line
- *
+ * 
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
-{
-	
+{	
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
