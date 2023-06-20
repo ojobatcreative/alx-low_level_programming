8c3d8312e8cfc650df5d93db305a475ab3d539f9
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-20 08:13:06 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet
+ * Description: A function that prints the alphabet,
+ * in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void print_alphabet(void)
+
+{
+	char pal;
+	
+	for (pal = 97; pal <= 122; pal++)
+	{
+		_putchar(pal);
+	}
+	_putchar('\n');
+}
