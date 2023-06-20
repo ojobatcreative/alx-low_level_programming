bf2bb9e1116f1ad1aefccc81c39b8b4947690c5f
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-20 08:38:16 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char pal;
+	int l = 0;
+
+	while (l <= 9)
+	{
+		for (pal = 97; pal <= 122; pal++)
+		{
+			_putchar(pal);
+		}
+			_putchar('\n');
+			l++;
+	}
+}
