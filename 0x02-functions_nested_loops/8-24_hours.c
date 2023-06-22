72afee4ea8f5bf81b0669b037694aa14af557ef9
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-21 21:45:18 -0700

    24 hours Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * Return: ...
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
  */
 
 void jack_bauer(void)
-
 {
-	int i, j, k, l;
+	int vi, fi;
 
-	for (i = 0; i <= 2; i++)
+	for (vi = 0; vi < 24; vi++)
 	{
-	for (j = 0; j <= 9; j++)
-	{
-	if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
-	{
-	for (k = 0; k <= 5; k++)
-	{
-	for (l = 0; l <= 9; l++)
-	_putchar(i + '0');
-	_putchar(j + '0');
-	_putchar(58);
-	_putchar(k + '0');
-	_putchar(l + '0');
-	_putchar('\n');
-	}
-	}
-	}
-	}
+		for (fi = 0; fi < 60; fi++)
+		{
+			_putchar((vi / 10) + '0');
+			_putchar((vi % 10) + '0');
+			_putchar(':');
+			_putchar((fi / 10) + '0');
+			_putchar((fi % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
