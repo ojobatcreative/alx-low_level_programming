9c49084aa2db0ccf0a30d7a752cbc2630d5c7d12
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-24 15:24:27 +0100

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,32 +9,17 @@
  */
 int main(void)
 {
-	long int t;
-	long int max;
-	long int i;
+	int t;
+	long num = 612852475143;
 
-	t = 612852475143;
-	max = -1;
-
-	while (t % 2 == 0)
+	for (t = (int) sqrt(num); t > 2; t++)
 	{
-		max = 2;
-		t /= 2;
-	}
-
-	for (i = 3; i <= sqrt(t); i = i + 2)
-	{
-		while (t % i == 0)
+		if (num % t == 0)
 		{
-			max = i;
-			t = t / i;
+			printf("%d\n", t);
+			break;
 		}
 	}
-
-	if (t > 2)
-		max = t;
-
-	printf("%ld\t", max);
 
 	return (0);
 }
