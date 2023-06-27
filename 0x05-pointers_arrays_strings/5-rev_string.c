9c75c0aebc441374dc8e097d551bf9e65d8e73e1
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-06-27 04:33:40 +0100

    print reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	char bat;
+	int j, lgt, lgt1;
+
+	lgt = 0;
+	lgt1 = 0;
+
+	while (s[lgt] != '\0')
+	{
+		lgt++;
+	}
+
+	lgt1 = lgt - 1;
+
+	for (j = 0; j < lgt / 2; j++)
+	{
+		bat = s[j];
+		s[j] = s[lgt1];
+		s[lgt1--] = bat;
+	}
+)
