f0dc0a267045b7e8fb5be96bd98a836cdac99415
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-01 07:23:06 +0100

    103-infinite_add.print buffer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,49 +1,72 @@
 #include "main.h"
 
 /**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int t = 0, u = 0; char tac;
+
+	while (*(n + t) != '\0')
+	{
+		t++;
+	}
+	t--;
+
+	for (u = 0; u < t; u++, t--)
+	{
+		tac = *(n + u);
+		*(n + u) = *(n + t);
+		*(n + t) = tac;
+	}
+}
+
+/**
  * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
-  * @n2: text representation of 2nd number to add
+ * @n1: text representation of 1st number
+ * @n2: text representation of 2nd number
  * @r: pointer to buffer
- * @size_r: buffer size
+ * @size_r: size of buffer
  * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
+	int ovlf = 0, t = 0, u = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-
-	if (j >= size_r || i >= size_r)
+	while (*(n1 + t) != '\0')
+		t++;
+	while (*(n2 + u) != '\0')
+		u++;
+	t--;
+	u--;
+	if (u >= size_r || t >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+	while (u >= 0 || t >= 0 || ovlf == 1)
 	{
-		if (i < 0)
+		if (t < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
+			val1 = *(n1 + t) - '0';
+		if (u < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
+			val2 = *(n2 + u) - '0';
+		temp_tot = val1 + val2 + ovlf;
 		if (temp_tot >= 10)
-			overflow = 1;
+			ovlf = 1;
 		else
-			overflow = 0;
+			ovlf = 0;
 		if (digits >= (size_r - 1))
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
-		j--;
-		i--;
+		u--;
+		t--;
 	}
 	if (digits == size_r)
 		return (0);
