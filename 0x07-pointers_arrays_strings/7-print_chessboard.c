b464e1b9fd795e7524b402722a51ea74a0cac09b
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-04 00:37:03 +0100

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c = o, z;
+
+	while (c < 8)
+	{
+		z = 0;
+		while (z < 8)
+		{
+			_putchar(a[c][z]);
+			z++;
+		}
+		_putchar('\n');
+		c++;
+	}
+}
