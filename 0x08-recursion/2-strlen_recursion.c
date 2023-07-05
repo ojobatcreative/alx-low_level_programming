c4f6e08db3880563991ac525358751eaee766d7e
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-06 00:43:09 +0100

    Lenght of String

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Return the length of a string.
+ * @s: The string.
+ * Return: The length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int leng_str = 0;
+
+	if (*s != '\0')
+	{
+		leng_str = 1;
+		if (*(s + 1) != '\0')
+			leng_str += _strlen_recursion(s + 1);
+	}
+
+	return leng_str;
+}
