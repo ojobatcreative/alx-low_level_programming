d016af8b21cfb349fbf77c0f49f1ec390db004ae
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-06 01:09:39 +0100

    returns the value os a given variable riased to a power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Returns the value of `x` raised to the power of `y`.
- * @x: The base value.
- * @y: The power value.
- * Return: The result of x raised to the power of y.
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
+ *
+ * Return: result of the power
  */
-
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
+	if (y == 0)
 		return (1);
-	{
-		int result = _pow_recursion(x, y / 2);
-		
-		if (y % 2 == 0)
-			return (result * result);
-		else
-			return (x * result * result);
-	}
+	return (x * _pow_recursion(x, y - 1));
 }
