4420ebaddde7de934e5c4807a99f69e6719c795b
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-06 21:02:01 +0100

    100 palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,47 @@
 #include "main.h"
 
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to check.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- * Return: 1 if the string is a palindrome, 0 if not.
+ * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
 {
-    int length = string_length(s);
-    return is_palindrome_recursive(s, 0, length - 1);
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- * string_length - Calculates the length of a string recursively.
- * @s: The string.
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
  *
- * Return: The length of the string.
+ * Return: length of the string
  */
-int string_length(char *s)
+int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return 0;
-    return 1 + string_length(s + 1);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * is_palindrome_recursive - Recursive helper function to check if a string is a palindrome.
- * @s: The string.
- * @start: The starting index.
- * @end: The ending index.
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
  *
- * Return: 1 if the string is a palindrome, 0 if not.
+ * Return: 1 if palindrome, 0 if not
  */
-int is_palindrome_recursive(char *s, int start, int end)
+int check_pal(char *s, int i, int len)
 {
-    if (start >= end)
-        return 1;
-    if (s[start] != s[end])
-        return 0;
-    return is_palindrome_recursive(s, start + 1, end - 1);
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
