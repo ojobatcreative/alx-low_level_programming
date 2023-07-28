aa79582f474eb82496dfde6fe623a5750183a106
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-28 08:58:32 +0100

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,11 +15,11 @@ size_t print_list(const list_t *h)
 	{
 	if (h->str == NULL)
 	{
-		printf("[0] (nil)\n");
+		printf("[%d] %s\n", 0, "(nil)");
 	}
 	else
 	{
-		printf("%s\n", h->str);
+		printf("[%d] %s\n", h->len, h->str);
 	}
 
 	cnt++;
