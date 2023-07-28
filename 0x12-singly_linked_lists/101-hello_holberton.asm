0952d76e04031b6383db93659387aa09c99d30dc
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-28 12:48:55 +0100

    print, Hello Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,22 @@
 section .data
-    message db 'Hello, Holberton', 0
-    format db '%s', 0
+	message db 'Hello, Holberton', 0
+	format db '%s', 0
 
 section .text
-    extern printf
-    
+	extern printf
+
 global _start
 
 _start:
-    sub rsp, 8
-    
-    mov rdi, format
-    mov rsi, message
-    xor eax, eax
-    call printf
-    
-    add rsp, 8
-    
-    mov eax, 60     ; exit syscall number
-    xor edi, edi    ; exit status code (0)
-    syscall
+	sub rsp, 8
+
+	mov rdi, format
+	mov rsi, message
+	xor eax, eax
+	call printf
+
+	add rsp, 8
+
+	mov eax, 60     ; exit syscall number
+	xor edi, edi    ; exit status code (0)
+	syscall
