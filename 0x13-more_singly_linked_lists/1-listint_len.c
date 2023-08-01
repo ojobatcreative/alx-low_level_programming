edd8f3072ff208947204dac2a0af81692bdc5cc9
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-08-02 00:33:31 +0100

    List lenght of int

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in
+ * a listint_t liked list
+ *
+ * @h: Pointer to the head node of the list.
+ * Return: The number of elements in the list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *pntr = h;
+	size_t nuelm = 0;
+
+	while (nuelm != NULL)
+	{
+		nuelm += 1;
+		pntr = nuelm->next;
+	}
+	return (nuelm);
+} 
