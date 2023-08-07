421463d6cd04fdb194db5189e5b07f8198678e2d
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-08-07 21:32:34 +0100

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,18 @@
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <elf.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
