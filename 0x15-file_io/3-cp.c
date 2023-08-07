1167aec56e0f18b3886dc0736db47169cf31b710
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-08-07 19:18:27 +0100

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,23 +28,20 @@ int main(int argc, char *argv[])
 	{
 		print_error_and_exit(97, "Usage : cp file_from file_to");
 	}
-
 	const char *file_from = argv[1];
 	const char *file_to = argv[2];
-
 	int file_des_from = open(file_from, O_RDONLY);
+
 	if (file_des_from == -1)
 	{
 		print_error_and_exit(98, strerror(errno));
 	}
-
 	int file_des_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
 	if (file_des_to == -1)
 	{
 		print_error_and_exit(99, strerror(errno));
 	}
-
-
 	char buffer[BUFFER_SIZE];
 	ssize_t read_byte, written_byte;
 
@@ -52,25 +49,19 @@ int main(int argc, char *argv[])
 	{
 		written_byte = write(file_des_to, buffer, read_byte);
 		if (written_byte == -1)
-		{
-			print_error_and_exit(99, strerror(errno));
-		}
+		print_error_and_exit(99, strerror(errno));
 	}
-
 	if (read_byte == -1)
 	{
 		print_error_and_exit(98, strerror(errno));
 	}
-
 	if (close(file_des_from) == -1)
 	{
 		print_error_and_exit(100, strerror(errno));
 	}
-
 	if (close(file_des_to) == -1)
 	{
 		print_error_and_exit(100, strerror(errno));
 	}
-
 	return (0);
 }
