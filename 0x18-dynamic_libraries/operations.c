f7802ee94ab3414ce46432b75cdad668be647790
Babatunde <babatundeojomu2016@gmail.com>
2023-11-16 12:30:12 +0100

    Using Shared library in Python that contains C functions

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+  * add - add two integers
+  * @a: first integer
+  * @b second integer
+  * Return: the Sum
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * sub - subtract two integers
+  * @a: first integer
+  * @b second integer
+  * Return: the difference
+  */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * mul - product of two integers
+  * @a: first integer
+  * @b second integer
+  * Return: the product
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * div - division of two integers
+  * @a: first integer
+  * @b second integer
+  * Return: the Answer
+  */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * mod - modulus of two integers
+  * @a: first integer
+  * @b second integer
+  * Return: the Modulus
+  */
+int mod(int a, int b)
+{
+	return (a % b);
+}
