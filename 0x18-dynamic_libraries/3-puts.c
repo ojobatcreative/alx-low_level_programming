afc609bb5064c88ab9facca05e0d76419e9a5817
Babatunde <babatundeojomu2016@gmail.com>
2023-11-16 09:56:59 +0100

    task 1 complete

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _puts - prints a string followed by a new line
--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+
 /**
  * _strpbrk - Entry point
  * @s: input
