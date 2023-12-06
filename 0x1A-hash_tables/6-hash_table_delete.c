d9ef8a0438833a5f93de261e8840645130b0cada
Babatunde <babatundeojomu2016@gmail.com>
2023-12-06 07:26:27 +0100

    Deletes a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int ti;
+
+	for (ti = 0; ti < ht->size; ti++)
+	{
+		if (ht->array[ti] != NULL)
+		{
+			node = ht->array[ti];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
